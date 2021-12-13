65dc87e877194d8e685359cf43b3b2ff9c0ac740
TianYuan <tianyuan.hu@tuya.com>
2021-12-13 18:01:03 +0800

    init

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -3,11 +3,14 @@ import { ReportHandler } from 'web-vitals';
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      const data = {
+        CLS: getCLS(onPerfEntry),
+        FID: getFID(onPerfEntry),
+        FCP: getFCP(onPerfEntry),
+        LCP: getLCP(onPerfEntry),
+        TTFB: getTTFB(onPerfEntry),
+      }
+      console.log('web-vitals-data', data)
     });
   }
 };
